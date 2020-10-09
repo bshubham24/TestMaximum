4fb9e4e995d4b12d45a329b09376c3376a6ef310
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-09 23:30:34 +0530

    [Shubham]Added: Print statement in maximum finding method

--- src/main/java/com/capgi/testmaximum/TestMaximum.java
+++ src/main/java/com/capgi/testmaximum/TestMaximum.java
@@ -17,9 +17,15 @@ public class TestMaximum<T extends Comparable<T>> {
 	}
 
 	public static <T extends Comparable<T>> T maximum(ArrayList<T> list) {
-		if (list.size() == 0)
+		if (list.size() == 0) {
+			System.out.println("The list is empty");
 			return null;
-		else
+		}
+
+		else {
+			System.out.println("Max: " + Collections.max(list));
 			return Collections.max(list);
+		}
+
 	}
 }
